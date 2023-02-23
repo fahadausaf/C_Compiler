a21e5bcd993dfdc3b938aa2e322612dfbceff51d
fahadausaf <fahad.ausaf@icloud.com>
2023-02-23 06:44:13 +0000

    6. Creating our token structures

--- compiler.h
+++ compiler.h
@@ -1,8 +1,82 @@
 #ifndef PEACHCOMPILER_H
 #define PEACHCOMPILER_H
 #include <stdio.h>
+#include <stdbool.h>
 
-enum{
+struct pos
+{
+    int line;
+    int col;
+    const char *filename;
+};
+
+enum
+{
+    TOKEN_TYPE_IDENTIFIER,
+    TOKEN_TYPE_KEYWORD,
+    TOKEN_TYPE_OPERATOR,
+    TOKEN_TYPE_SYMBOL,
+    TOKEN_TYPE_NUMBER,
+    TOKEN_TYPE_STRING,
+    TOKEN_TYPE_COMMENT,
+    TOKEN_TYPE_NEWLINE
+};
+
+struct token
+{
+    int type;
+    int flags;
+
+    union
+    {
+        char cval;
+        const char *sval;
+        unsigned int inum;
+        unsigned long lnum;
+        unsigned long long llnum;
+        void *any;
+    };
+
+    // True if there is whitespace between the token and the next token
+    // i.e * a for operator token * would mean whitespace would be set for token "a"
+    bool whitespace;
+
+    // (HELLO World)
+    const char *between_brackets;
+};
+
+struct lex_process;
+typedef char (*LEX_PROCESS_NEXT_CHAR)(struct lex_process *process);
+typedef char (*LEX_PROCESS_PEEK_CHAR)(struct lex_process *process);
+typedef void (*LEX_PROCESS_PUSH_CHAR)(struct lex_process *process, char c);
+
+struct lex_process_functions
+{
+    LEX_PROCESS_NEXT_CHAR next_char;
+    LEX_PROCESS_PEEK_CHAR peek_char;
+    LEX_PROCESS_PUSH_CHAR push_char;
+};
+
+struct lex_process
+{
+    struct pos pos;
+    struct vector *token_vec;
+    struct compile_process *compiler;
+
+    /*
+    ((50))
+    */
+    int current_expression_count;
+    struct buffer *parenthesis_buffer;
+    struct lex_process_functions *function;
+
+    // This will be private data that the lexer does not understand
+    // but the person using the lexer does undertand
+    void *private;
+};
+
+enum
+{
     COMPILER_FILE_COMPILED_OK,
     COMPILER_FAILED_WITH_ERRORS
 };
@@ -18,7 +92,7 @@ struct compile_process
         const char *abs_path;
     } cfile;
 
-    FILE* ofile;
+    FILE *ofile;
 };
 
 int compile_file(const char *filename, const char *out_filename, int flags);
