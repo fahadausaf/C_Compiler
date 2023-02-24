01adf446549a449895225b4afc4cc898f2e7da74
Fahad Ausaf <fahad.ausaf@icloud.com>
2023-02-24 15:13:31 +0000

    lexer setup

--- compiler.c
+++ compiler.c
@@ -1,5 +1,11 @@
 #include "compiler.h"
 
+struct lex_process_functions compiler_lex_functions = {
+    .next_char=compile_process_next_char,
+    .peek_char=compile_process_peek_char,
+    .push_char=compile_process_push_char
+};
+
 int compile_file(const char *filename, const char *out_filename, int flags)
 {
     struct compile_process* process = compile_process_create(filename, out_filename, flags);
@@ -7,6 +13,7 @@ int compile_file(const char *filename, const char *out_filename, int flags)
         return COMPILER_FAILED_WITH_ERRORS;
 
     // Perform Lexical Analysis
+    struct lex_process* lex_process = lex_process_create(process, &compiler_lex_functions, NULL);
 
     // Perform Parsing
 
--- compiler.h
+++ compiler.h
@@ -86,6 +86,7 @@ struct compile_process
     // The flags in regards to how this file should be compiled
     int flags;
 
+    struct pos pos;
     struct compile_process_input_file
     {
         FILE *fp;
@@ -97,5 +98,9 @@ struct compile_process
 
 int compile_file(const char *filename, const char *out_filename, int flags);
 struct compile_process *compile_process_create(const char *filename, const char *filename_out, int flags);
+
+char compile_process_next_char(struct lex_process* lex_process);
+char compile_process_peek_char(struct lex_process* lex_process);
+void compile_process_push_char(struct lex_process* lex_process);
 
 #endif
